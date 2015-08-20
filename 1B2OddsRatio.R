b6074e92c5045a218376db2fbaa70c7704d6aee0
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-08-20 15:09:04 -0700

    non-binary data analysis

--- 1B2OddsRatio.R
+++ 1B2OddsRatio.R
@@ -18,40 +18,6 @@ library(PredictABEL) #for adjusted odds ratio
 #call for entire 1B2 data
 total <- read.csv(file.path("1B2metabac.csv"))
 
-#adjusted odds ratio example
-install.packages("PredictABEL", dependencies = TRUE)
-library(PredictABEL)
-data(ExampleData)
-# specify column number of outcome variable
-cOutcome <- 2
-# specify column numbers of non-genetic predictors
-cNonGenPred <- c(3:10)
-# specify column numbers of non-genetic predictors that are categorical
-cNonGenPredCat <- c(6:8)
-# specify column numbers of genetic predictors
-cGenPred <- c(11,13:16)
-# specify column numbers of genetic predictors that are categorical
-cGenPredCat <- c(0)
-riskmodel <- fitLogRegModel(data=total, cOutcome=cOutcome,
-                            cNonGenPreds=cNonGenPred, cNonGenPredsCat=cNonGenPredCat,
-                            cGenPreds=cGenPred, cGenPredsCat=cGenPredCat)
-#categorize variables (ie 19-25 = 1 etc.)
-# obtain multivariate OR(95% CI) for all predictors of the fitted model
-a <- ORmultivariate(riskModel=riskmodel, filename="multiOR.txt")
-a <- as.data.frame(a)
-
-#apply to own data
-# specify column number of outcome variable
-cOutcome <- 2
-# specify column numbers of non-genetic predictors
-cNonGenPred <- c(4,5,38, 39:43,68:94)
-# specify column numbers of non-genetic predictors that are categorical
-cNonGenPredCat <- c(9,10)
-# specify column numbers of genetic predictors
-cGenPred <- c(0)
-# specify column numbers of genetic predictors that are categorical
-cGenPredCat <- c(0)
-
 #Odss Ratio figured out. Need to put data into categories
 #data has to be in factor form
 total$Symptoms..y.1..n.0. <- factor(total$Symptoms..y.1..n.0.)
@@ -64,7 +30,7 @@ mylogit <- glm(formula = Nugent.score ~ Symptoms..y.1..n.0. +
                family = binomial(link = "logit"))
 mylogit <- glm(formula = Nugent.score ~ Symptoms..y.1..n.0. + 
                  abnormal.discharge..y.1..n.0., data = total, 
-               family = binomial)
+               family = binomial) #gives same results thus far
 
 mylogit
 confint(mylogit) #CI intervals
@@ -72,3 +38,24 @@ exp(cbind(OR = coef(mylogit), confint(mylogit))) #ORs and CIs
 exp(coef(mylogit)) #only ORs
 summary(mylogit)# for really nice table
 #cannot convert glm into data.frame
+
+#above is binary data, below may be able to deal with non-binary data
+
+#odds ratio
+## needs epitools package
+tapw <- c("Lowest", "Intermediate", "Highest") #example
+outc <- c("Case", "Control")
+dat <- matrix(c(2, 29, 35, 64, 12, 6),3,2,byrow=TRUE)
+dimnames(dat) <- list("Tap water exposure" = tapw, "Outcome" = outc)
+oddsratio(dat, rev="c")
+oddsratio.midp(dat, rev="c")
+oddsratio.fisher(dat, rev="c")
+oddsratio.wald(dat, rev="c")
+oddsratio.small(dat, rev="c")
+
+# the zeros are probably a problem
+t <- c("lacto", "no lacto")
+o <- c("no", "inter", "yes")
+dat <- matrix(c(64, 1601, 69, 0, 0, 0),2,3,byrow=TRUE)
+dimnames(dat) <- list("Lacto presence" = t, "Outcome" = o)
+oddsratio(dat, rev="c")
