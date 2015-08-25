287338a09bade1e34e70bab36e2ffb7f3bdea5d4
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-08-25 16:34:14 -0700

    more categories

--- 1B2Categories.R
+++ 1B2Categories.R
@@ -10,7 +10,8 @@ total$Nugent.score.cat[Nugent.score > 3 & Nugent.score <= 6] <- "1" #intermediat
 total$Nugent.score.cat[Nugent.score <= 3] <- "0" #negative
 detach(total)
 
-#OR (but then need to sort into categories)(will not use this)
+#OR (but then need to sort into categories)
+#only use this when column is a factor #see Ethnicity
 total$Nugent.score.cat <- mapply(as.factor, total$Nugent.score)
 
 #convert Nugent.score.cat from character into factor
@@ -39,11 +40,49 @@ total$Age.cat <- factor(total$Age.cat)
 
 #BMI
 attach(total)
-total$Age.cat[Age < 20] <- "1" 
-total$Age.cat[Age >= 20 & Age <=29] <- "2"
-total$Age.cat[Age >= 30 & Age <=39] <- "3"
-total$Age.cat[Age >= 40] <- "4" 
+total$BMI.cat[BMI < 18.5] <- "1" #underweight
+total$BMI.cat[BMI >= 18.5 & BMI <=24.9] <- "2" #normal weight
+total$BMI.cat[BMI >= 25 & BMI <=29.9] <- "3" #overweight
+total$BMI.cat[BMI >= 30] <- "4" #obesity
 detach(total)
 
-#convert Age.cat from character into factor
-total$Age.cat <- factor(total$Age.cat)
+#convert BMI.cat from character into factor
+total$BMI.cat <- factor(total$BMI.cat)
+
+#Ethinicty
+#needs to be character to reassign value
+total$Ethnicity.cat <- mapply(as.character, total$Ethnicity)
+#create categories
+total$Ethnicity.cat[total$Ethnicity.cat=='Caucasian'] <- '1'
+total$Ethnicity.cat[total$Ethnicity.cat=='Asian'] <- '2'
+total$Ethnicity.cat[total$Ethnicity.cat=='Aboriginal'] <- '3'
+total$Ethnicity.cat[total$Ethnicity.cat=='Other (Arab)'] <- '4'
+total$Ethnicity.cat[total$Ethnicity.cat=='Other (Haida/Scottish)'] <- '4'
+
+#convert Ethnicity.cat from character into factor
+total$Ethnicity.cat <- factor(total$Ethnicity.cat)
+
+#Martial Status
+#needs to be character to reassign value
+total$Marital.Status.cat <- mapply(as.character, total$Marital.Status)
+#create categories
+total$Marital.Status.cat[total$Marital.Status.cat=='Single'] <- '1'
+total$Marital.Status.cat[total$Marital.Status.cat=='Married/Common Law'] <- '2'
+total$Marital.Status.cat[total$Marital.Status.cat=='Other'] <- '3'
+total$Marital.Status.cat[total$Marital.Status.cat=='Other (engaged)'] <- '3'
+
+#convert Marital.Status.cat from character into factor
+total$Marital.Status.cat <- factor(total$Marital.Status.cat)
+
+#Highest Education level attained
+#needs to be character to reassign value
+total$Highest.Education.Level.cat <- mapply(as.character, total$Highest.Education.Level)
+#create categories
+total$Highest.Education.Level.cat[total$Highest.Education.Level.cat=='did not complete highschool'] <- '1'
+total$Highest.Education.Level.cat[total$Highest.Education.Level.cat=='Highschool'] <- '2'
+total$Highest.Education.Level.cat[total$Highest.Education.Level.cat=='some post-secondary'] <- '3'
+total$Highest.Education.Level.cat[total$Highest.Education.Level.cat=='post-secondary'] <- '4'
+total$Highest.Education.Level.cat[total$Highest.Education.Level.cat=='graduate degree'] <- '5'
+
+#convert Ethnicity.cat from character into factor
+total$Highest.Education.Level.cat <- factor(total$Highest.Education.Level.cat)
