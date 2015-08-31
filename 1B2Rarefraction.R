5e2b9a4f93b36c8dc6d354e937ea5065cec6c181
KeshiniD <kdevakan@alumni.uwo.ca>
2015-08-30 18:10:38 -0700

    participant labels

--- 1B2Rarefraction.R
+++ 1B2Rarefraction.R
@@ -76,7 +76,7 @@ bac2 <-
          Other.Clostridium, Other.Firmicutes, Other.Lactobacillus, 
          Other.Prevotella, Other.Proteobacteria, Other.Streptococcus)
 bac3 <- bac2 %>% 
-  select (Bacteria, Counts) %>% #WORKED!!!! for cohort
+  select (Bacteria, Counts) %>% 
   group_by(Bacteria) %>%
   summarize(TotalCounts = sum(Counts)) %>%
   select (TotalCounts)
@@ -90,13 +90,31 @@ rarefy2 <- rarefy(bac3, sample=min(rowSums(bac3))) #may be h
 rarefy2 <- as.data.frame(rarefy2)
 write.csv(rarefy2, "1B2richness_cohort.csv")
 
-#need to figure out colours; done
+#Plot rarefraction cureve
+bac <- data %>%
+  select(Participants, Lactobacillus.crispatus, Lactobacillus.gasseri, 
+         Lactobacillus.iners, Lactobacillus.jensenii, 
+         Gardnerella.vaginalis.Group.A, Gardnerella.vaginalis.Group.B, 
+         Gardnerella.vaginalis.Group.C, Gardnerella.vaginalis.Group.D, 
+         Actinobacteria.sp., Atopobium.vaginae, Clostridia.sp..BVAB2, 
+         Clostridium.genomosp..BVAB3, Escherichia.coli, 
+         Klebsiella.pneumoniae, Megasphaera.sp..genomosp..type.1, 
+         Prevotella.amnii, Prevotella.timonensis, Streptococcus.devriesei, 
+         Other.Actinobacteria, Other.Bacteria, Other.Bacteroidetes, 
+         Other.Clostridium, Other.Firmicutes, Other.Lactobacillus, 
+         Other.Prevotella, Other.Proteobacteria, Other.Streptococcus)
+#want rownames to be participants
+rownames(total) <- total[,1]
+#colours for individuals
 col <- c("black", "darkred", "forestgreen", "orange", "blue", "yellow", 
          "hotpink", "red", "grey", "purple", "white")
-rarecurve(bac, step = 1, sample = 2, xlab = "Sample Size", ylab = "Species",
+rarecurve(bac, step = 1, sample = min(rowSums(bac)), xlab = "Sample Size", ylab = "Species",
           label = TRUE, col = col, xlim=c(0,15000)) # will plot, set sample and step to integer
+#each line is rarefied richness value (higher values more rich than lower values)
+#xaxis:# of indvidual species present in each participant
+#yaxis: bacterial species and amount each participant has
 #need to figure out legend (do not think there is one)
 #can alter x-axis to see some samples more distinctly 
 
 fisher.alpha(bac, MARGIN = 1)
-specnumber(bac, MARGIN = 1) #works for H5
+specnumber(bac, MARGIN = 1) #number of species, can use above for cohort
