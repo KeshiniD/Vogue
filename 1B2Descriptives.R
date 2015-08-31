5037b7cd5ec768e87a0208c236ed0a9df2e0c6c7
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-08-31 15:51:58 -0700

    more descriptives about population

--- 1B2Descriptives.R
+++ 1B2Descriptives.R
@@ -42,9 +42,63 @@ sd(total$How.often.pain.experienced.during.vaginal.intercourse.percentage, na.rm
 summary(total$Contraception)
 
 #genital infections
+#BV
 mean(total$BV..number.of.episodes.2.months., na.rm=TRUE)
 range(total$BV..number.of.episodes.2.months., na.rm=TRUE)
 sd(total$BV..number.of.episodes.2.months., na.rm=TRUE)
 mean(total$BV..number.of.episodes.year., na.rm=TRUE)
 range(total$BV..number.of.episodes.year., na.rm=TRUE)
 sd(total$BV..number.of.episodes.year., na.rm=TRUE)
+mean(total$BV..number.of.episodes.lifetime., na.rm=TRUE)
+range(total$BV..number.of.episodes.lifetime., na.rm=TRUE)
+sd(total$BV..number.of.episodes.lifetime., na.rm=TRUE)
+
+#Yeast
+mean(total$Yeast..2months., na.rm=TRUE)
+range(total$Yeast..2months., na.rm=TRUE)
+sd(total$Yeast..2months., na.rm=TRUE)
+mean(total$Yeast..year., na.rm=TRUE)
+range(total$Yeast..year., na.rm=TRUE)
+sd(total$Yeast..year., na.rm=TRUE)
+mean(total$Yeast..lifetime., na.rm=TRUE)
+range(total$Yeast..lifetime., na.rm=TRUE)
+sd(total$Yeast..lifetime., na.rm=TRUE)
+
+#UTI
+mean(total$UTI..2.months., na.rm=TRUE)
+range(total$UTI..2.months., na.rm=TRUE)
+sd(total$UTI..2.months., na.rm=TRUE)
+mean(total$UTI..year., na.rm=TRUE)
+range(total$UTI..year., na.rm=TRUE)
+sd(total$UTI..year., na.rm=TRUE)
+mean(total$UTI..lifetime., na.rm=TRUE)
+range(total$UTI..lifetime., na.rm=TRUE)
+sd(total$UTI..lifetime., na.rm=TRUE)
+
+#
+summary(total$Nugent.score.cat)
+summary(total$Freq.of.Menstrual.Period)
+summary(total$Tampon.Use)
+summary(total$Use.of.douche.products..y.1..n.0.)
+summary(total$Use.of.feminine.wipes.or.genital.deodrant..y.1..n.0.)
+summary(total$use.of.drugs..y.1..n.0.)
+summary(total$alcohol.use..y.1..n.0.)
+summary(total$smoker..current.or.in.past...y.1..n.0.)
+summary(total$Sexual.Partners)
+
+#
+mean(total$Number.partners.in.past.2.months, na.rm=TRUE)
+range(total$Number.partners.in.past.2.months, na.rm=TRUE)
+sd(total$Number.partners.in.past.2.months, na.rm=TRUE)
+mean(total$Number.partners.in.past.year, na.rm=TRUE)
+range(total$Number.partners.in.past.year, na.rm=TRUE)
+sd(total$Number.partners.in.past.year, na.rm=TRUE)
+
+#
+summary(total$Vaginal.intercourse.in.past.48.hours..y.1..n.0.)
+summary(total$Freq.oral.sex)
+summary(total$Freq.anal.sex)
+summary(total$Freq.sex.toy.use)
+
+#
+summary(total$CST)
