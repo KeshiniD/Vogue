d7cf174a1ddc567fa308ed0dd401cc2582199b90
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-11-03 11:41:06 -0800

    create code

--- 1B2Contingency_SD.R
+++ 1B2Contingency_SD.R
@@ -1,1 +1,19 @@
+#load packages
+library(vegan)
+library(plyr)
+##suppresses start up messages
+suppressPackageStartupMessages(library(dplyr)) 
+library(ggplot2)
+library(tidyr)
+library(knitr)
+library(assertthat)
+library(entropart)
+library(epitools)
+library(ggtree)
+library(PredictABEL) #for adjusted odds ratio
+
+#call for entire 1B2 data
+total <- read.csv(file.path("1B2metabac_condensed.csv"))
+
 #need to separate SD into cat
+mean(total$Shannon.s.Diversity)
