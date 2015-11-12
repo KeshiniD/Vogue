c29c628a0540dfc538aaca0f43e28ed92e75e37d
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-11-12 12:26:39 -0800

    SD cohort

--- 1A_Subset_Analyses.R
+++ 1A_Subset_Analyses.R
@@ -87,3 +87,92 @@ H <- dplyr::rename(H, ShannonsDiversity = H, Participants = VALUE)
 write.table(H, "1A_subset_individual_diversity.csv", sep = ",", row.names = FALSE, quote = FALSE)
 div <- read.csv(file.path("1A_subset_individual_diversity.csv"))
 
+#SD cohort
+#first need to make new table with total counts for each bacterial species
+#load dataset
+data <- read.csv(file.path("data1A_1B2.csv"))
+
+#column into row labels
+data2 <- data[,-1]
+rownames(data2) <- data[,1]
+
+#select subset of participants wish to analyze
+data2 <- data2 %>%
+  select(Vogue1A.01.003, Vogue1A.01.007, Vogue1A.01.012, 
+         Vogue1A.01.016, Vogue1A.01.017, Vogue1A.01.019, Vogue1A.01.020, 
+         Vogue1A.01.022, Vogue1A.01.023, Vogue1A.01.024, Vogue1A.01.025, 
+         Vogue1A.01.026, Vogue1A.01.029, Vogue1A.01.030, Vogue1A.01.033, 
+         Vogue1A.01.034, Vogue1A.01.036, Vogue1A.01.041, Vogue1A.01.042, 
+         Vogue1A.01.046, Vogue1A.01.049, Vogue1A.01.054, Vogue1A.01.060, 
+         Vogue1A.01.061, Vogue1A.01.063, Vogue1A.01.066, Vogue1A.01.067, 
+         Vogue1A.01.072, Vogue1A.01.073, Vogue1A.01.077, Vogue1A.01.078, 
+         Vogue1A.01.079, Vogue1A.01.082, Vogue1A.01.085, Vogue1A.01.089, 
+         Vogue1A.01.090, Vogue1A.01.092, Vogue1A.01.095, Vogue1A.01.097, 
+         Vogue1A.01.099, Vogue1A.01.100, Vogue1A.01.107, Vogue1A.01.112, 
+         Vogue1A.01.113, Vogue1A.01.114, Vogue1A.01.118, Vogue1A.01.123, 
+         Vogue1A.01.125, Vogue1A.01.126, Vogue1A.01.127, Vogue1A.01.128, 
+         Vogue1A.01.130, Vogue1A.01.134, Vogue1A.01.135, Vogue1A.01.137, 
+         Vogue1A.01.139, Vogue1A.01.140, Vogue1A.01.142, Vogue1A.01.145, 
+         Vogue1A.01.146, Vogue1A.01.147, Vogue1A.01.150, Vogue1A.01.152, 
+         Vogue1A.01.156, Vogue1A.01.157, Vogue1A.01.161, Vogue1A.01.162, 
+         Vogue1A.01.163, Vogue1A.01.166, Vogue1A.01.169, Vogue1A.01.170, 
+         Vogue1A.01.172, Vogue1A.01.173, Vogue1A.01.174, Vogue1A.01.175, 
+         Vogue1A.01.184, Vogue1A.01.186, Vogue1A.01.187, Vogue1A.01.191, 
+         Vogue1A.01.192, Vogue1A.01.197, Vogue1A.01.198, Vogue1A.01.200, 
+         Vogue1A.01.201, Vogue1A.01.203, Vogue1A.01.205, Vogue1A.01.207, 
+         Vogue1A.01.208, Vogue1A.01.211, Vogue1A.01.212, Vogue1A.01.213, 
+         Vogue1A.01.214, Vogue1A.01.215, Vogue1A.01.216, Vogue1A.01.217, 
+         Vogue1A.01.218, Vogue1A.01.219, Vogue1A.01.224, Vogue1A.01.227, 
+         Vogue1A.01.231, Vogue1A.01.233, Vogue1A.01.236, Vogue1A.01.237, 
+         Vogue1A.01.238)
+
+#transpose dataset
+data2 <- as.data.frame(t(data2))
+#row into column
+data2 <- add_rownames(data2, "Participants")
+
+#write data to file 
+write.table(data2, "1A_temp.csv", sep = ",", row.names = FALSE, quote = FALSE)
+data2 <- read.csv(file.path("1A_temp.csv"))
+
+
+#bac counts
+data3 <-
+  gather(data2, key = 'Bacteria', value = 'Counts', Actinobacteria.sp., 
+         Alloscardovia.omnicolens, Atopobium.vaginae, 
+         Bifidobacterium.breve, Clostridia.sp..BVAB2, 
+         Clostridium.genomosp..BVAB3, Enterococcus.rattus, 
+         Escherichia.coli, Eukaryote, Gardnerella.vaginalis.Group.A, 
+         Gardnerella.vaginalis.Group.B, Gardnerella.vaginalis.Group.C, 
+         Gardnerella.vaginalis.Group.D, Klebsiella.pneumoniae, 
+         Lactobacillus.crispatus, Lactobacillus.gasseri, 
+         Lactobacillus.iners, Lactobacillus.jensenii, 
+         Megasphaera.sp..genomosp..type.1, Other.Actinobacteria, 
+         Other.Bacteria, Other.Bacteroidetes, Other.Bifidobacterium, 
+         Other.Clostridium, Other.Firmicutes, Other.Lactobacillus, 
+         Other.Prevotella, Other.Proteobacteria, Other.Streptococcus, 
+         Porphyromonas.uenonis, Prevotella.amnii, Prevotella.timonensis, 
+         Pseudomonas.putida, Streptococcus.devriesei, Variovorax.paradoxus)
+
+#then use in diversity
+H2 <- data3 %>% 
+  select (Bacteria, Counts) %>% #WORKED!!!! for cohort
+  group_by(Bacteria) %>%
+  summarize(TotalCounts = sum(Counts)) %>%
+  select (TotalCounts) #stay in for diversity,can remove to see bac
+F2 <- diversity(H2)
+View(F2)
+
+#remove the absent bacterial species and see if this alters SD
+H2 <- H2[-c(1,8,14), ]
+F3 <- diversity(H2)
+View(F3) #exactly the same
+F3 <- as.data.frame(F3) #data.frame
+
+#rename headers
+F3 <- dplyr::rename(F3, ShannonsDiversity = F3)
+
+#write data to file 
+write.table(F3, "1A_subset_cohort_diversity.csv", sep = ",", row.names = FALSE, quote = FALSE)
+cdiv <- read.csv(file.path("1A_subset_cohort_diversity.csv"))
+
