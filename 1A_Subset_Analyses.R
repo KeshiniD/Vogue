f82518d61af0b224ef1bccef03e912171e853841
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-11-12 11:16:06 -0800

    pwr calculation

---
+++ 1A_Subset_Analyses.R
@@ -0,0 +1,10 @@
+library(pwr)
+pwr.t2n.test(n1 = 26, n2=104 , sig.level = 0.05, power = 0.8)
+# t test power calculation 
+#n1 = 26
+#n2 = 104
+#d = 0.6189457 (can detect medium differences)
+#sig.level = 0.05
+#power = 0.8
+#alternative = two.sided
+
