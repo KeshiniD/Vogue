1c76328234eee47bc6b9c3317f273b71974749fd
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-11-12 11:18:04 -0800

    proportions pwr calc

--- 1A_Subset_Analyses.R
+++ 1A_Subset_Analyses.R
@@ -8,3 +8,12 @@ pwr.t2n.test(n1 = 26, n2=104 , sig.level = 0.05, power = 0.8)
 #power = 0.8
 #alternative = two.sided
 
+pwr.2p2n.test(n1 = 26, n2=104 , sig.level = 0.05, power = 0.8)
+#difference of proportion power calculation for binomial distribution (arcsine transformation) 
+#h = 0.6142887 (can detect medium differences)
+#n1 = 26
+#n2 = 104
+#sig.level = 0.05
+#power = 0.8
+#alternative = two.sided
+#NOTE: different sample sizes
