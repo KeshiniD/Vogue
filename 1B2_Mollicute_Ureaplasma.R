dc16c8f61e1b7430a0832217e1b26998a73cd6f1
Keshini Devakandan <kdevakan@alumni.uwo.ca>
2015-12-01 11:17:26 -0800

    mollicutes/ureaplasma cat and plots

--- 1B2_Mollicute_Ureaplasma.R
+++ 1B2_Mollicute_Ureaplasma.R
@@ -7,20 +7,45 @@ library(ggplot2)
 #load dataset
 data <- read.csv(file.path("1B2_Mollicute_Ureaplasma.csv"))
 
+
 #rename variables
-data <- dplyr::rename(data, 16s.category = X16s_category, #:/
-                      16s.copies.per.swab = X16s_copies_per_swab)
+#data <- dplyr::rename(data, '16s_copies_per_swab' = X16s_copies_per_swab, 
+#                      '16s_category' = X16s_category)
+#error because cannot start with a number, unless put in quotes"
+#still presents problems when try to call for variable
+
+
+#make new cat which indicate positive and negative, and dif ureaplasma
+#Mollicutes
+#rename
+data <- dplyr::rename(data, Mollicutes.cat = mollicutes, 
+                      Ureaplasma.cat = ureaplasma)
+
+#new mollicute.cat
+data$Mollicutes[data$Mollicutes.cat <= 1 ] <- "Positive" 
+data$Mollicutes[data$Mollicutes.cat >= 2] <- "Negative" 
+
+#convert Mollicutes from character into factor
+data$Mollicutes <- factor(data$Mollicutes)
+
+#new ureaplasma.cat
+data$Ureaplasma[data$Ureaplasma.cat <= 1 ] <- "Negative" 
+data$Ureaplasma[data$Ureaplasma.cat <= 2 & 
+                  data$Ureaplasma.cat >= 2] <- "Parvum" 
+data$Ureaplasma[data$Ureaplasma.cat >= 3] <- "Urealyticum" 
+
+#convert Ureaplasma from character into factor
+data$Ureaplasma <- factor(data$Ureaplasma)
 
 #make variables into factors
-data$mollicutes <- factor(data$mollicutes)
-data$ureaplasma <- factor(data$ureaplasma)
-data$X16s_category <- factor(data$X16s_category) #new name
-data$X16s_copies_per_swab <- factor(data$X16s_copies_per_swab) #new name
+data$Mollicutes.cat <- factor(data$Mollicutes.cat)
+data$Ureaplasma.cat <- factor(data$Ureaplasma.cat)
+data$X16s_category <- factor(data$X16s_category)
 
 #summary of each variables
 summary(data$mollicutes)
 summary(data$ureaplasma)
-summary(data$X16s_category) #new name
+summary(data$X16s_category)
 
 #Mollicutes 
 #1-positive: 18
@@ -35,12 +60,37 @@ summary(data$X16s_category) #new name
 #5-not tested: 2
 
 #16s cat
-#1-<10˄4
-#2-10˄4
-#3-10˄5
-#4-10˄6
-#5-10˄7
-#6-10˄8
-#7-10˄9
-#8->10˄9
+#1-<10˄: 0
+#2-10˄4: 0
+#3-10˄5: 0
+#4-10˄6: 0
+#5-10˄7: 4
+#6-10˄8: 9
+#7-10˄9: 10
+#8->10˄9: 1
+#NAs: 2
 
+#plots so far; same species are not together
+#need to order ureaplasma to solve this
+data2 <- data %>%
+  arrange(Ureaplasma)
+#use data2 below
+
+#plot mollicute and ureaplasma for participants
+ggplot(data = data2, aes(x = Participants, y = Mollicutes, 
+                        fill = Ureaplasma)) + 
+  geom_bar(stat = "identity") + coord_flip() + 
+  ylab("Mollicutes") +
+    ggtitle("Mollicutes and Ureaplasma of the Vaginal Microbiome of Women with Recurrent Bacterial Vaginosis")
+#drop NAs from plot
+
+#facet_wrap mollicutes, and show ureaplasma in each cat.
+ggplot(data = data2, aes(x = factor(1), y = Ureaplasma, 
+                        fill = Ureaplasma)) + 
+  geom_bar(stat = "identity", width = 1) + ylab("Ureaplasma") +
+  ggtitle("Cpn60 Species Characterization of the Vaginal Microbiome of Women with Recurrent Bacterial Vaginosis") + 
+  facet_wrap(~Mollicutes)
+
+
+#pie chart
+coord_polar(theta="y")
