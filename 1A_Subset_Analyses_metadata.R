2ffe586fad6463a3833a51b623bb797b7c41dfa2
KeshiniD <kdevakan@alumni.uwo.ca>
2016-02-24 00:54:04 -0800

    subset data and group variables

--- 1A_Subset_Analyses_metadata.R
+++ 1A_Subset_Analyses_metadata.R
@@ -1,4 +1,3 @@
-##Diversity Indices
 #load packages
 library(vegan)
 library(plyr)
@@ -13,136 +12,117 @@ library(epitools)
 #load dataset
 data <- read.csv(file.path("VOGUE_1A.csv"))
 
+#participants randomly selected as seen in "1A_randomized_selection.csv"
 
-#grouping variables
+### DEAN ADDED THIS
+#does what the multiple lines below does so deleted them
+dean <- read.csv("../Vogue/1A_randomized_selection.csv")
+nums <- substring(dean$Study.ID, 12)
+ids <- paste0("Vogue 1A 01-", nums)
+total <- data[which(data$study_id %in% ids), ]
+###
 
-
-############
+############################
+#Grouping variables
 #Age
-#Age
-attach(total)
-total$Age.cat[Age < 20] <- "1" 
-total$Age.cat[Age >= 20 & Age <=29] <- "2"
-total$Age.cat[Age >= 30 & Age <=39] <- "3"
-total$Age.cat[Age >= 40] <- "4" 
-detach(total)
+total$Age.cat[total$age < 20] <- "1" 
+total$Age.cat[total$age >= 20 & total$age <=29] <- "2"
+total$Age.cat[total$age >= 30] <- "3" 
+#total$Age.cat[total$Age >= 30 & Age <=39] <- "3" #combo 30-39 wiht 40+
+#total$Age.cat[total$Age >= 40] <- "3" 
 
 #convert Age.cat from character into factor
-total$Age.cat <- factor(total$Age.cat)
-#combine 30-39 with 40+
-data$Age.cat[data$Age.cat > 3] <- "3" #reassinging 40+ to 30s cat
-#convert Age.cat to factor
-data$Age.cat <- as.factor(data$Age.cat)
-
+total$Age.cat <- as.factor(total$Age.cat)
 
 ###############
 #BMI 
 #Two categories; compare underweight to normal and overweight/obese to normal
-
 #Underweight and Normal weight
-data$BMI.under.cat[data$BMI < 18.5] <- "1" #underweight
-data$BMI.under.cat[data$BMI >= 18.5 & data$BMI <=24.9] <- "2" #normal weight
-data$BMI.under.cat[data$BMI >= 25] <- "" #overweight/obese
+total$BMI.under.cat[total$bmi < 18.5] <- "1" #underweight
+total$BMI.under.cat[total$bmi >= 18.5 & total$bmi <=24.9] <- "2" #normal weight
+total$BMI.under.cat[total$bmi >= 25] <- "" #overweight/obese
 
 #convert BMI.cat from character into factor
-data$BMI.under.cat <- factor(data$BMI.under.cat) 
+total$BMI.under.cat <- as.factor(total$BMI.under.cat) 
 
 #OVerweight/Obese and Normal weight
-data$BMI.over.cat[data$BMI < 18.5] <- "" #underweight
-data$BMI.over.cat[data$BMI >= 18.5 & data$BMI <=24.9] <- "2" #normal weight
-data$BMI.over.cat[data$BMI >= 25] <- "3" #overweight/obese
+total$BMI.over.cat[total$bmi < 18.5] <- "" #underweight
+total$BMI.over.cat[total$bmi >= 18.5 & total$bmi <=24.9] <- "2" #normal weight
+total$BMI.over.cat[total$bmi >= 25] <- "3" #overweight/obese
 
 #convert BMI.cat from character into factor
-data$BMI.over.cat <- factor(data$BMI.over.cat) 
+total$BMI.over.cat <- factor(total$BMI.over.cat) 
 
 ###########
 #Ethnicity 
 #combine Other, Aboriginal, and Asian
-data$Ethnicity.cat[data$Ethnicity=='Caucasian'] <- '1'
-data$Ethnicity.cat[data$Ethnicity=='Asian'] <- '2'
-data$Ethnicity.cat[data$Ethnicity=='Aboriginal'] <- '2'
-data$Ethnicity.cat[data$Ethnicity=='Other (Arab)'] <- '2'
-data$Ethnicity.cat[data$Ethnicity=='Other (Haida/Scottish)'] <- '2'
+total$Ethnicity.cat[total$ethwhite___1>=1] <- '1'
+total$Ethnicity.cat[total$ethblack___1>=1] <- '2'
+total$Ethnicity.cat[total$ethhisp___1>=1] <- '2'
+total$Ethnicity.cat[total$ethasian___1>=1] <- '2'
+total$Ethnicity.cat[total$ethsasian___1>=1] <- '2'
+total$Ethnicity.cat[total$ethfirstnation___1>=1] <- '2'
+total$Ethnicity.cat[total$ethother___1>=1] <- '2'
+
 #convert Ethnicity.cat from character into factor
-data$Ethnicity.cat <- factor(data$Ethnicity.cat)
+total$Ethnicity.cat <- factor(total$Ethnicity.cat)
 
 ############
 #leave BV and Yeast episodes alone
 ############
 #Genital Infections
 #collapse UTI category, UTI ever
-total$UTI.ever <- ifelse(total$UTI..lifetime. > 0, 
+total$UTI.ever <- ifelse(total$uti_life > 0, 
                          c("1"), c("0")) 
 #convert UTI.ever from character into factor
 total$UTI.ever <- factor(total$UTI.ever) 
-#remove other UTI categories
-total$UTI..2.months. <- NULL
-total$UTI..year. <- NULL
-total$UTI..lifetime. <- NULL
 
 #collapse Trich category, Trich ever
-total$Trich.ever <- ifelse(total$Trich..lifetime.=='chronic', 
+total$Trich.ever <- ifelse(total$trich_life > 0, 
                            c("1"), c("0")) 
 #convert Trich.ever from character into factor
 total$Trich.ever <- factor(total$Trich.ever) 
-#remove other TRich categories
-total$Trich..2.months. <- NULL
-total$Trich..year. <- NULL
-total$Trich..lifetime. <- NULL
 
 #collapse Genital Warts category, GenWarts ever
-total$Genwarts.ever <- ifelse(total$Genital.Warts..lifetime. > 0, 
+total$Condyloma.ever <- ifelse(total$condy_life > 0, 
                               c("1"), c("0")) 
 #convert Genwarts.ever from character into factor
-total$Genwarts.ever <- factor(total$Genwarts.ever) 
-#remove other UTI categories
-total$Genital.Warts..2months. <- NULL
-total$Genital.Warts..year. <- NULL
-total$Genital.Warts..lifetime. <- NULL   
+total$Condyloma.ever <- factor(total$Condyloma.ever) 
 
 #collapse Genital Herpes category, GenHerpes ever
-total$GenHerpes.ever <- ifelse(total$Genital.Herpes..lifetime. > 0, 
+total$GenHerpes.ever <- ifelse(total$herpes_infecttotal_life > 0, 
                                c("1"), c("0")) 
 #convert GenHerpes.ever from character into factor
 total$GenHerpes.ever <- factor(total$GenHerpes.ever) 
-#remove other UTI categories
-total$Genital.Herpes..2months. <- NULL
-total$Genital.Herpes..year. <- NULL
-total$Genital.Herpes..lifetime. <- NULL   
 
-#collapse Genital Herpes category, GenHerpes ever
-total$GenHerpes.ever <- ifelse(total$Genital.Herpes..lifetime. > 0, 
+#collapse Chlamydia category, Chlamydia ever
+total$Chlamydia.ever <- ifelse(total$chlam_life > 0, 
                                c("1"), c("0")) 
-#convert GenHerpes.ever from character into factor
-total$GenHerpes.ever <- factor(total$GenHerpes.ever) 
-#remove other UTI categories
-total$Genital.Herpes..2months. <- NULL
-total$Genital.Herpes..year. <- NULL
-total$Genital.Herpes..lifetime. <- NULL   
+#convert Chlamydia.ever from character into factor
+total$Chlamydia.ever <- factor(total$Chlamydia.ever) 
 
-#already have chlamydia ever category
-summary(total$Chlamydia.ever)
-#remove other chlamydia categories
-total$Chlamydia..2.months. <- NULL
-total$Chlamydia..year. <- NULL
-total$Chlamydia..lifetime. <- NULL   
+#collapse Gonorrhea category, Gonorrhea ever
+total$Gonorrhea.ever <- ifelse(total$gonor_life > 0, 
+                               c("1"), c("0")) 
+#convert Gonorrhea.ever from character into factor
+total$Gonorrhea.ever <- factor(total$Gonorrhea.ever) 
 
-total$Gonorrhea.ever #no levels
-#remove and syphillis too
-total$Gonorrhea <- NULL
-total$Gonorrhea.ever <- NULL 
-total$Syphillis <- NULL
+#collapse Syphillis category, Syphillis ever
+total$Syphillis.ever <- ifelse(total$syph_life > 0, 
+                               c("1"), c("0")) 
+#convert Syphillis.ever from character into factor
+total$Syphillis.ever <- factor(total$Syphillis.ever) 
 
 #####################
 #convert integers into factors
 #Antimicrobial use in the past 3 months
-# yes-1, no-0
-total$Antimicrobial.Use..y.1..n.0. <- factor(total$Antimicrobial.Use..y.1..n.0.)
+# yes-1, no-2
+total$antimicrodrug <- factor(total$antimicrodrug)
 
 #convert integers into factors
 #(non)Prescription use in the past 2 months
-# yes-1, no-0
-total$X.Non..Prescription..y.1..n.0. <- factor(total$X.Non..Prescription..y.1..n.0.)
+# yes-1, no-2
+total$rxdrug <- factor(total$rxdrug)
 ########################
 
 #manually created
@@ -150,98 +130,114 @@ total$X.Non..Prescription..y.1..n.0. <- factor(total$X.Non..Prescription..y.1..n
 total[,"probiotics.2.months"]  <- c(0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,
                                     0,0,0,1,1,0,0,0,1)
 #convert numeric into factor
-# yes-1, no-0
+# yes-1, no-2
 total$probiotics.2.months <- factor(total$probiotics.2.months)
 #######################
 
 #Symptoms
 #create for 48hrs and 2 weeks
 #add new column for presence of symptoms in 2 weeks
-total[,"Presence.Symptoms.2wks"]  <- c(1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,
-                                       1,1,1,1,1,1,1,1)
+total$Presence.Symptoms.2wks <- ifelse(total$abnormaldischarge2wk > 0 |
+                                         total$abnormalodor2wk > 0 |
+                                         total$irritationdiscomfort2wk > 0 |
+                                         total$vaginalsymptomother2wk > 0, 
+                               c("1"), c("2")) 
+
 #convert numeric into factor
-# yes-1, no-0
+# yes-1, no-2
 total$Presence.Symptoms.2wks <- factor(total$Presence.Symptoms.2wks)
+
+#add new column for presence of symptoms in 48hrs
+total$Presence.Symptoms.48hrs <- ifelse(total$abnormaldischarge48 > 0 |
+                                         total$abnormalodor48 > 0 |
+                                         total$irritationdiscomfort48 > 0 |
+                                         total$vaginalsymptomother48 > 0, 
+                                       c("1"), c("2")) 
+
+#convert numeric into factor
+# yes-1, no-2
+total$Presence.Symptoms.48hrs <- factor(total$Presence.Symptoms.48hrs)
 ###################
 
-#create symptoms pain variable
 #sxpain
-summary(total$How.often.pain.experienced.during.vaginal.intercourse.percentage)
-#condense to yes and no
-total$Symptom.pain <- ifelse(total$How.often.pain.experienced.during.vaginal.intercourse.percentage > 0, 
-                             c("1"), c("0")) 
-#convert Substance.Use from character into factor
-total$Symptom.pain <- factor(total$Symptom.pain)
+#convert sx.pain from character into factor
+total$Symptom.pain <- factor(total$vagintercoursediscomfort)
 summary(total$Symptom.pain)
+
 ########################
 #convert integers into factors
 #vaginal intercourse in the past 48 hours
-# yes-1, no-0
-total$Vaginal.intercourse.in.past.48.hours..y.1..n.0. <- factor(total$Vaginal.intercourse.in.past.48.hours..y.1..n.0.)
+# yes-1, no-2
+total$vaginalintercourse48hr <- factor(total$vaginalintercourse48hr)
 ##########################
 
 #Frequency of Oral Sex
 #create categories (change to never-ever cats)
-#data$Freq.oral.sex.cat[data$Freq.oral.sex.cat=='never'] <- '0'
-data$Freq.oral.sex.cat[data$Freq.oral.sex.cat >0] <- '1' #need only run this
+total$oralsxfrequency.cat[total$oralsxfrequency < 6] <- '1' #yes-1, no-2
+total$oralsxfrequency.cat[total$oralsxfrequency > 5] <- '2' #yes-1, no-2
 
 #convert Freq.oral.sex.cat from character into factor
-data$Freq.oral.sex.cat <- factor(data$Freq.oral.sex.cat)
+total$oralsxfrequency.cat <- as.factor(total$oralsxfrequency.cat)
 
-#same for anal and sex toy
+#Frequency of Anal Sex
+#create categories (change to never-ever cats)
+total$analsxfrequency.cat[total$analsxfrequency < 6] <- '1' #yes-1, no-2
+total$analsxfrequency.cat[total$analsxfrequency > 5] <- '2' #yes-1, no-2
+
+#convert Freq.anal.sex.cat from character into factor
+total$analsxfrequency.cat <- as.factor(total$analsxfrequency.cat)
+
+#Frequency of Sex Toy Use
+#create categories (change to never-ever cats)
+total$sextoyfrequency.cat[total$sextoyfrequency < 6] <- '1' #yes-1, no-2
+total$sextoyfrequency.cat[total$sextoyfrequency > 5] <- '2' #yes-1, no-2
+
+#convert Freq.sex.toy.cat from character into factor
+total$sextoyfrequency.cat <- as.factor(total$sextoyfrequency.cat)
+
 ##########################
 
 #Sexual Partners
+#male-1, female-2, both-3, virgin-4
 #needs to be character to reassign value
-total$Sexual.Partners.cat <- mapply(as.character, total$Sexual.Partners)
-#create categories
 total$Sexual.Partners.cat[total$Sexual.Partners.cat=='Male'] <- '0'
 total$Sexual.Partners.cat[total$Sexual.Partners.cat=='Female'] <- '1'
 
 #convert Sexual.Partners.cat from character into factor
 total$Sexual.Partners.cat <- factor(total$Sexual.Partners.cat)
-#seemed to have removed this cat; nvm
-#remove Sexual Partners (LATER)
-summary(total$Sexual.Partners.cat)
-#Remove non-cat variables
-total$Sexual.Partners <- NULL
 
 ###########################################
 #change to never-ever cats. 
 ##Cat. for number of sexual partners in the last year
-data$Number.partners.in.past.year.cat[data$Number.partners.in.past.year <= 0] <- "0" # 0 partners
-data$Number.partners.in.past.year.cat[data$Number.partners.in.past.year >= 1] <- "1"#1 or more partners
+total$sexpartner1yr.cat[total$sexpartner1yr <= 0] <- "2" # 0 partners
+total$sexpartner1yr.cat[total$sexpartner1yr >= 1] <- "1"#1 or more partners
 
 #convert Number.partners.in.past.year.cat from character into factor
-data$Number.partners.in.past.year.cat <- factor(data$Number.partners.in.past.year.cat)
+total$sexpartner1yr.cat <- factor(total$sexpartner1yr.cat)
 
 ################################
 #Contraception
-summary(total$Contraception)
-#condense categories (none-S, B, H and IUD): y-n categories
-summary(total$contraception.H) #keep
-summary(total$contraception.B.M) #keep
-summary(total$contraception.C.IUD)#keep
-#make new 'no contraception category
-total$Contraception.none <- ifelse(total$Contraception=='None' | total$Contraception=='S', 
-                                   c("1"), c("0")) 
-#convert Contraception.none from character into factor
-total$Contraception.none <- factor(total$Contraception.none)
-summary(total$Contraception.none)
+#condense categories (none-S, B, H and IUD): y-n categories (1-2)
+total$Contraception.H <- ifelse(total$contramethhormonal___1 >=1, 
+                                        c("1"), c("2")) 
 
-#remove the remaining categories
-total$Contraception <- NULL
-total$Contraception.cat <- NULL
-total$contraception.S.S <- NULL
-total$contraception.S.P <- NULL
-total$contraception.B.F <- NULL
-total$HContr.Progestin.pill <- NULL
-total$HContr.Combination.pill <- NULL
-total$HContr.nuvaring <- NULL
-total$HContr.mirena <- NULL
-total$HContr.depoprovera <- NULL
-total$HContr.orthoevra <- NULL
-total$contr_type <- NULL
+total$Contraception.B.M <- ifelse(total$contramethbarriermc___1 >=1, 
+                                c("1"), c("2")) 
+
+total$Contraception.IUD <- ifelse(total$contramethbarrieriud___1 >=1, 
+                                  c("1"), c("2")) 
+
+total$Contraception.none <- ifelse(total$contramethnone___1>=1 | 
+                                     total$contramethsurgstersubj___1>=1 |
+                                     total$contramethsurgsterpart___1>=1, 
+                                   c("1"), c("2")) 
+
+#convert Contraception cats from character into factor
+total$Contraception.H <- factor(total$Contraception.H)
+total$Contraception.B.M <- factor(total$Contraception.B.M)
+total$Contraception.IUD <- factor(total$Contraception.IUD)
+total$Contraception.none <- factor(total$Contraception.none)
+
 ###########################################
 
 #Use of condoms in last 48 hours
@@ -253,8 +249,8 @@ total$condoms.48h <- factor(total$condoms.48h)
 ############################################
 #Condense pregnancy into null vs multi
 # multi-1, null-0
-total$Pregnancy.History..g.
-total$Pregnancy.cat <- ifelse(total$Pregnancy.History..g. > 1, 
+total$pregnancyhistoryg
+total$Pregnancy.cat <- ifelse(total$pregnancyhistoryg > 1, 
                               c("1"), c("0")) 
 #convert Pregnancy.cat from character into factor
 total$Pregnancy.cat <- factor(total$Pregnancy.cat) 
@@ -262,45 +258,30 @@ summary(total$Pregnancy.cat)
 ###############################################
 
 #douche products and feminine wipes
-summary(total$Use.of.douche.products..y.1..n.0.)
-summary(total$Use.of.feminine.wipes.or.genital.deodrant..y.1..n.0.)
-#combine today variables into feminine product use
-total$Feminine.products <- ifelse(total$Use.of.douche.products..y.1..n.0. == '1' |
-                                    total$Use.of.feminine.wipes.or.genital.deodrant..y.1..n.0. == '1', 
-                                  c("1"), c("0")) 
+#combine two variables into feminine product use
+total$Feminine.products <- ifelse(total$doucheproducts == '1' |
+                                    total$deodorantproducts == '1', 
+                                  c("1"), c("2")) 
 total$Feminine.products <- factor(total$Feminine.products) 
 summary(total$Feminine.products)
 
-#remove douche and feminine variables
-total$Use.of.douche.products..y.1..n.0.<- NULL
-total$Use.of.feminine.wipes.or.genital.deodrant..y.1..n.0.<- NULL
-
 #used feminine products in past 48 hours
-summary(total$Used.in.the.past.48.hours)#douche
-summary(total$Used.in.past.48.hours)#feminine wipes
-#combine today variables into feminine product use
-total$Feminine.products.48hrs <- ifelse(total$Used.in.the.past.48.hours == '1' |
-                                          total$Used.in.past.48.hours == '1', 
-                                        c("1"), c("0")) 
+#combine two variables into feminine product use
+total$Feminine.products.48hrs <- ifelse(total$douche48hrs == '1' |
+                                          total$deodorant48hrs == '1', 
+                                        c("1"), c("2")) 
 total$Feminine.products.48hrs <- factor(total$Feminine.products.48hrs) 
 summary(total$Feminine.products.48hrs)
 
-#remove douche and feminine variables past 48hours
-total$Used.in.the.past.48.hours<- NULL
-total$Used.in.past.48.hours<- NULL
 ##########################################################
 
 #Frequency of Tampon Use
-#needs to be character to reassign value
-data$Tampon.Use.cat <- mapply(as.character, data$Tampon.Use)
 #create categories (never-ever cats)
-data$Tampon.Use.cat[data$Tampon.Use.cat=='never'] <- '1'#never used
-data$Tampon.Use.cat[data$Tampon.Use.cat=='sometimes but not for every period'] <- '2'#used ever
-data$Tampon.Use.cat[data$Tampon.Use.cat=='every period, part of the time'] <- '2'
-data$Tampon.Use.cat[data$Tampon.Use.cat=='every period, exclusively'] <- '2'
+total$Tampon.Use.cat <- ifelse(total$tamponusage == '4', 
+                                        c("2"), c("1")) 
 
 #convert Tampon.Use.cat from character into factor
-data$Tampon.Use.cat <- factor(data$Tampon.Use.cat)
+total$Tampon.Use.cat <- factor(total$Tampon.Use.cat)
 
 ##########################################################
 #Days since LMP
@@ -315,9 +296,9 @@ summary(total$Tampon.Use)
 total$days.since.LMP
 #menses within 30 days, do they use tampon every period (exclusively or partly)
 total$Tampon.use.1mth <- ifelse(total$days.since.LMP <30 & 
-                                  total$Tampon.Use =='every period, exclusively' | 
-                                  total$Tampon.Use =='every period, part of the time', 
-                                c("1"), c("0")) 
+                                  total$tamponusage<=2, 
+                                c("1"), c("2")) 
+
 #convert Tampon.use.1mth from character into factor
 total$Tampon.use.1mth <- factor(total$Tampon.use.1mth)
 summary(total$Tampon.use.1mth)
@@ -325,19 +306,16 @@ summary(total$Tampon.use.1mth)
 
 #Substance use
 #change to never-ever cats
-data$Substance.Use[data$Substance.Use >0] <- '1' #emcompass those who use past or currently
-
+#emcompasses those who use past or currently
 #convert Substance.Use from character into factor
-data$Substance.Use <- factor(data$Substance.Use)
+total$substance_use_yn <- factor(total$substance_use_yn)
 
 ##############################################################
 
-#smoking
-summary(total$smoker..current.or.in.past...y.1..n.0.)
-summary(total$smoker..current.or.in.past...y.1..n.0.cat)
+#smoking (yes-1, no-2)
 #condense to currently smoking or not
-total$smoking.current <- ifelse(total$smoker..current.or.in.past...y.1..n.0. == 'current', 
-                                c("1"), c("0")) 
+total$smoking.current <- ifelse(total$tobaccouse == '2', 
+                                c("1"), c("2")) 
 #convert Substance.Use from character into factor
 total$smoking.current <- factor(total$smoking.current)
 summary(total$smoking.current)
