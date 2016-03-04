b22f826c74083926f81dee548e35bd9d762e3de6
KeshiniD <kdevakan@alumni.uwo.ca>
2016-03-04 02:37:13 -0800

    analyses

---
+++ 1B2Fishers.R
@@ -0,0 +1,240 @@
+#load dataset
+total <- read.csv(file = "1B2metabac_condensedv2.csv")
+
+#load packages
+library(vegan)
+library(plyr)
+##suppresses start up messages
+suppressPackageStartupMessages(library(dplyr)) 
+library(ggplot2)
+library(tidyr)
+library(knitr)
+library(vcd)
+
+#convert variables into factors
+
+total$Age.cat <- factor(total$Age.cat)
+total$BMI.cat <- factor(total$BMI.cat)
+total$Ethnicity.cat <- factor(total$Ethnicity.cat)
+total$Antimicrobial.Use..y.1..n.0. <- factor(total$Antimicrobial.Use..y.1..n.0.)
+total$X.Non..Prescription..y.1..n.0. <- factor(total$X.Non..Prescription..y.1..n.0.)
+total$Tampon.Use.cat <- factor(total$Tampon.Use.cat)
+total$Vaginal.intercourse.in.past.48.hours..y.1..n.0. <- factor(total$Vaginal.intercourse.in.past.48.hours..y.1..n.0.)
+total$Presence.Symptoms.2wks <- factor(total$Presence.Symptoms.2wks)
+total$Abnormal.discharge.2wks <- factor(total$Abnormal.discharge.2wks)
+total$Abnormal.odor.2wks <- factor(total$Abnormal.odor.2wks)
+total$Irritation.Discomfort.2wks <- factor(total$Irritation.Discomfort.2wks)
+total$Other.Symptoms.2wks <- factor(total$Other.Symptoms.2wks)
+total$Presence.Symptoms.48hrs <- factor(total$Presence.Symptoms.48hrs)
+total$Abnormal.discharge.48hrs <- factor(total$Abnormal.discharge.48hrs)
+total$Abnormal.odor.48hrs <- factor(total$Abnormal.odor.48hrs)
+total$Irritation.Discomfort.48hrs <- factor(total$Irritation.Discomfort.48hrs)
+total$Other.Symptoms.48hrs <- factor(total$Other.Symptoms.48hrs)
+total$contraception.H <- factor(total$contraception.H)
+total$contraception.B.M <- factor(total$contraception.B.M)
+total$contraception.C.IUD <- factor(total$contraception.C.IUD)
+total$condoms.48h <- factor(total$condoms.48h)
+total$probiotics.2.months <- factor(total$probiotics.2.months)
+total$CST <- factor(total$CST)
+total$CSTI <- factor(total$CSTI)
+total$CSTII <- factor(total$CSTII)
+total$CSTIII <- factor(total$CSTIII)
+total$CSTIVA <- factor(total$CSTIVA)
+total$CSTIVC <- factor(total$CSTIVC)
+total$CSTIVD <- factor(total$CSTIVD)
+total$Sexual.Partners.cat <- factor(total$Sexual.Partners.cat)
+total$Freq.oral.sex.cat <- factor(total$Freq.oral.sex.cat)
+total$Freq.anal.sex.cat <- factor(total$Freq.anal.sex.cat)
+total$Freq.sex.toy.use.cat <- factor(total$Freq.sex.toy.use.cat)
+total$Chlamydia.ever <- factor(total$Chlamydia.ever)
+total$Genwarts.ever <- factor(total$Genwarts.ever)
+total$Number.partners.in.past.year.cat <- factor(total$Number.partners.in.past.year.cat)
+total$UTI.ever <- factor(total$UTI.ever)
+total$Trich.ever <- factor(total$Trich.ever)
+total$GenHerpes.ever <- factor(total$GenHerpes.ever)
+total$Pregnancy.cat <- factor(total$Pregnancy.cat)
+total$Feminine.products <- factor(total$Feminine.products)
+total$Feminine.products.48hrs <- factor(total$Feminine.products.48hrs)
+total$Substance.Use <- factor(total$Substance.Use)
+total$smoking.current <- factor(total$smoking.current)
+total$Symptom.pain <- factor(total$Symptom.pain)
+total$Contraception.none <- factor(total$Contraception.none)
+total$Tampon.use.1mth <- factor(total$Tampon.use.1mth)
+
+#2X2 tables-Fishers (cat-cat)
+#pvalue <0.05 reject null and there is assocation
+#pvalue >0.05 do not reject null and there is no association
+#effect size; 0.1 small, 0.3 medium, 0.5 large
+
+#Demographics
+a <- xtabs(~Age.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~BMI.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+#BMI
+a <- xtabs(~BMI.under.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~BMI.over.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Ethnicity.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+#genital infections
+a <- xtabs(~UTI.ever + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Chlamydia.ever + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Genwarts.ever + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Trich.ever + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~GenHerpes.ever + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+#meds
+a <- xtabs(~Antimicrobial.Use..y.1..n.0. + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~X.Non..Prescription..y.1..n.0. + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~probiotics.2.months + CST, data = total)
+fisher.test(a)
+assocstats(a)
+
+#symptoms
+a <- xtabs(~Presence.Symptoms.2wks + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Abnormal.discharge.2wks + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Abnormal.odor.2wks + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Irritation.Discomfort.2wks + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Other.Symptoms.2wks + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Presence.Symptoms.48hrs + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Abnormal.discharge.48hrs + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Abnormal.odor.48hrs + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Irritation.Discomfort.48hrs + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Other.Symptoms.48hrs + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+a <- xtabs(~Symptom.pain + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+#Sexual Activity
+a <- xtabs(~Vaginal.intercourse.in.past.48.hours..y.1..n.0. + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Freq.oral.sex.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Freq.anal.sex.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Freq.sex.toy.use.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Sexual.Partners.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Number.partners.in.past.year.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~contraception.H + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~contraception.B.M + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~contraception.C.IUD + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Contraception.none + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~condoms.48h + CST , data = total)
+fisher.test(a)
+assocstats(a)
+#Pregnancy
+a <- xtabs(~Pregnancy.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+#Product use
+a <- xtabs(~Feminine.products + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Feminine.products.48hrs + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Tampon.Use.cat + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~Tampon.use.1mth + CST , data = total)
+fisher.test(a)
+assocstats(a)
+#substance use
+a <- xtabs(~Substance.Use + CST , data = total)
+fisher.test(a)
+assocstats(a)
+a <- xtabs(~smoking.current + CST , data = total)
+fisher.test(a)
+assocstats(a)
+
+#one-way anova (cont-2+cat)
+#pvalue <0.05 reject null and there is assocation
+#pvalue >0.05 do not reject null and there is no association
+summary(aov(total$BV..number.of.episodes.2.months. ~total$CST))
+summary(aov(total$BV..number.of.episodes.year. ~total$CST))
+summary(aov(total$BV..number.of.episodes.lifetime. ~total$CST))
+
+summary(aov(total$Yeast..2months. ~total$CST))
+summary(aov(total$Yeast..year. ~total$CST))
+summary(aov(total$Yeast..lifetime. ~total$CST))
+
+aggregate(total$Contraception.none, list(total$CST), summary)
---
+++ 1B2_SD.R
@@ -0,0 +1,201 @@
+#load dataset
+total <- read.csv(file = "1B2metabac_condensedv2.csv")
+
+#load packages
+library(vegan)
+library(plyr)
+##suppresses start up messages
+suppressPackageStartupMessages(library(dplyr)) 
+library(ggplot2)
+library(tidyr)
+library(knitr)
+library(vcd)
+
+#convert variables into factors
+
+total$Age.cat <- factor(total$Age.cat)
+total$BMI.cat <- factor(total$BMI.cat)
+total$Ethnicity.cat <- factor(total$Ethnicity.cat)
+total$Antimicrobial.Use..y.1..n.0. <- factor(total$Antimicrobial.Use..y.1..n.0.)
+total$X.Non..Prescription..y.1..n.0. <- factor(total$X.Non..Prescription..y.1..n.0.)
+total$Tampon.Use.cat <- factor(total$Tampon.Use.cat)
+total$Vaginal.intercourse.in.past.48.hours..y.1..n.0. <- factor(total$Vaginal.intercourse.in.past.48.hours..y.1..n.0.)
+total$Presence.Symptoms.2wks <- factor(total$Presence.Symptoms.2wks)
+total$Abnormal.discharge.2wks <- factor(total$Abnormal.discharge.2wks)
+total$Abnormal.odor.2wks <- factor(total$Abnormal.odor.2wks)
+total$Irritation.Discomfort.2wks <- factor(total$Irritation.Discomfort.2wks)
+total$Other.Symptoms.2wks <- factor(total$Other.Symptoms.2wks)
+total$Presence.Symptoms.48hrs <- factor(total$Presence.Symptoms.48hrs)
+total$Abnormal.discharge.48hrs <- factor(total$Abnormal.discharge.48hrs)
+total$Abnormal.odor.48hrs <- factor(total$Abnormal.odor.48hrs)
+total$Irritation.Discomfort.48hrs <- factor(total$Irritation.Discomfort.48hrs)
+total$Other.Symptoms.48hrs <- factor(total$Other.Symptoms.48hrs)
+total$contraception.H <- factor(total$contraception.H)
+total$contraception.B.M <- factor(total$contraception.B.M)
+total$contraception.C.IUD <- factor(total$contraception.C.IUD)
+total$condoms.48h <- factor(total$condoms.48h)
+total$probiotics.2.months <- factor(total$probiotics.2.months)
+total$CST <- factor(total$CST)
+total$CSTI <- factor(total$CSTI)
+total$CSTII <- factor(total$CSTII)
+total$CSTIII <- factor(total$CSTIII)
+total$CSTIVA <- factor(total$CSTIVA)
+total$CSTIVC <- factor(total$CSTIVC)
+total$CSTIVD <- factor(total$CSTIVD)
+total$Sexual.Partners.cat <- factor(total$Sexual.Partners.cat)
+total$Freq.oral.sex.cat <- factor(total$Freq.oral.sex.cat)
+total$Freq.anal.sex.cat <- factor(total$Freq.anal.sex.cat)
+total$Freq.sex.toy.use.cat <- factor(total$Freq.sex.toy.use.cat)
+total$Chlamydia.ever <- factor(total$Chlamydia.ever)
+total$Genwarts.ever <- factor(total$Genwarts.ever)
+total$Number.partners.in.past.year.cat <- factor(total$Number.partners.in.past.year.cat)
+total$UTI.ever <- factor(total$UTI.ever)
+total$Trich.ever <- factor(total$Trich.ever)
+total$GenHerpes.ever <- factor(total$GenHerpes.ever)
+total$Pregnancy.cat <- factor(total$Pregnancy.cat)
+total$Feminine.products <- factor(total$Feminine.products)
+total$Feminine.products.48hrs <- factor(total$Feminine.products.48hrs)
+total$Substance.Use <- factor(total$Substance.Use)
+total$smoking.current <- factor(total$smoking.current)
+total$Symptom.pain <- factor(total$Symptom.pain)
+total$Contraception.none <- factor(total$Contraception.none)
+total$Tampon.use.1mth <- factor(total$Tampon.use.1mth)
+
+
+#simple linear regression
+#Shannon's Diversity
+cor.test(~total$Shannon.s.Diversity + total$BV..number.of.episodes.2.months.,  
+         method = c("pearson"))
+
+cor.test(~total$Shannon.s.Diversity + total$BV..number.of.episodes.year.,  
+         method = c("pearson"))
+
+cor.test(~total$Shannon.s.Diversity + total$BV..number.of.episodes.lifetime.,  
+         method = c("pearson"))
+
+cor.test(~total$Shannon.s.Diversity + total$Yeast..2months.,  
+         method = c("pearson"))
+
+cor.test(~total$Shannon.s.Diversity + total$Yeast..year.,  
+         method = c("pearson"))
+
+cor.test(~total$Shannon.s.Diversity + total$Yeast..lifetime.,  
+         method = c("pearson"))
+
+#t.test
+#Shannon's Diversity
+#Demographics
+t.test(Shannon.s.Diversity~Age.cat, data = total)
+cohen.d(Shannon.s.Diversity~Age.cat, data = total)
+
+
+t.test(Shannon.s.Diversity~BMI.under.cat, data = total)
+cohen.d(Shannon.s.Diversity~BMI.under.cat, data = total)
+t.test(Shannon.s.Diversity~BMI.over.cat, data = total)
+cohen.d(Shannon.s.Diversity~BMI.over.cat, data = total)
+t.test(Shannon.s.Diversity~Ethnicity.cat, data = total)
+cohen.d(Shannon.s.Diversity~Ethnicity.cat, data = total)
+
+#genital infections
+t.test(Shannon.s.Diversity~UTI.ever, data = total)
+cohen.d(Shannon.s.Diversity~UTI.ever, data = total)
+t.test(Shannon.s.Diversity~Chlamydia.ever, data = total)
+cohen.d(Shannon.s.Diversity~Chlamydia.ever, data = total)
+t.test(Shannon.s.Diversity~Genwarts.ever, data = total)
+cohen.d(Shannon.s.Diversity~Genwarts.ever, data = total)
+t.test(Shannon.s.Diversity~Trich.ever, data = total)
+cohen.d(Shannon.s.Diversity~Trich.ever, data = total)
+t.test(Shannon.s.Diversity~GenHerpes.ever, data = total)
+cohen.d(Shannon.s.Diversity~GenHerpes.ever, data = total)
+
+#meds
+t.test(Shannon.s.Diversity~Antimicrobial.Use..y.1..n.0., data = total)
+cohen.d(Shannon.s.Diversity~Antimicrobial.Use..y.1..n.0., data = total)
+t.test(Shannon.s.Diversity~X.Non..Prescription..y.1..n.0., data = total)
+cohen.d(Shannon.s.Diversity~X.Non..Prescription..y.1..n.0., data = total)
+t.test(Shannon.s.Diversity~probiotics.2.months, data = total)
+cohen.d(Shannon.s.Diversity~probiotics.2.months, data = total)
+
+#symptoms
+t.test(Shannon.s.Diversity~Presence.Symptoms.2wks, data = total)
+cohen.d(Shannon.s.Diversity~Presence.Symptoms.2wks, data = total)
+t.test(Shannon.s.Diversity~Abnormal.discharge.2wks, data = total)
+cohen.d(Shannon.s.Diversity~Abnormal.discharge.2wks, data = total)
+t.test(Shannon.s.Diversity~Abnormal.odor.2wks, data = total)
+cohen.d(Shannon.s.Diversity~Abnormal.odor.2wks, data = total)
+t.test(Shannon.s.Diversity~Irritation.Discomfort.2wks, data = total)
+cohen.d(Shannon.s.Diversity~Irritation.Discomfort.2wks, data = total)
+t.test(Shannon.s.Diversity~Other.Symptoms.2wks, data = total)
+cohen.d(Shannon.s.Diversity~Other.Symptoms.2wks, data = total)
+t.test(Shannon.s.Diversity~Presence.Symptoms.48hrs, data = total)
+cohen.d(Shannon.s.Diversity~Presence.Symptoms.48hrs, data = total)
+t.test(Shannon.s.Diversity~Abnormal.discharge.48hrs, data = total)
+cohen.d(Shannon.s.Diversity~Abnormal.discharge.48hrs, data = total)
+t.test(Shannon.s.Diversity~Abnormal.odor.48hrs, data = total)
+cohen.d(Shannon.s.Diversity~Abnormal.odor.48hrs, data = total)
+t.test(Shannon.s.Diversity~Irritation.Discomfort.48hrs, data = total)
+cohen.d(Shannon.s.Diversity~Irritation.Discomfort.48hrs, data = total)
+t.test(Shannon.s.Diversity~Other.Symptoms.48hrs, data = total)
+cohen.d(Shannon.s.Diversity~Other.Symptoms.48hrs, data = total)
+t.test(Shannon.s.Diversity~Symptom.pain, data = total)
+cohen.d(Shannon.s.Diversity~Symptom.pain, data = total)
+
+#Sexual Activity
+t.test(Shannon.s.Diversity~Vaginal.intercourse.in.past.48.hours..y.1..n.0., data = total)
+cohen.d(Shannon.s.Diversity~Vaginal.intercourse.in.past.48.hours..y.1..n.0., data = total)
+t.test(Shannon.s.Diversity~Freq.oral.sex.cat, data = total)
+cohen.d(Shannon.s.Diversity~Freq.oral.sex.cat, data = total)
+t.test(Shannon.s.Diversity~Freq.anal.sex.cat, data = total)
+cohen.d(Shannon.s.Diversity~Freq.anal.sex.cat, data = total)
+t.test(Shannon.s.Diversity~Freq.sex.toy.use.cat, data = total)
+cohen.d(Shannon.s.Diversity~Freq.sex.toy.use.cat, data = total)
+t.test(Shannon.s.Diversity~Sexual.Partners.cat, data = total)
+cohen.d(Shannon.s.Diversity~Sexual.Partners.cat, data = total)
+t.test(Shannon.s.Diversity~Number.partners.in.past.year.cat, data = total)
+cohen.d(Shannon.s.Diversity~Number.partners.in.past.year.cat, data = total)
+
+#Contraception
+
+t.test(Shannon.s.Diversity~contraception.H, data = total)
+cohen.d(Shannon.s.Diversity~contraception.H, data = total)
+
+t.test(Shannon.s.Diversity~contraception.B.M, data = total)
+cohen.d(Shannon.s.Diversity~contraception.B.M, data = total)
+
+t.test(Shannon.s.Diversity~contraception.C.IUD, data = total)
+cohen.d(Shannon.s.Diversity~contraception.C.IUD, data = total)
+
+t.test(Shannon.s.Diversity~Contraception.none, data = total)
+cohen.d(Shannon.s.Diversity~Contraception.none, data = total)
+
+t.test(Shannon.s.Diversity~condoms.48h, data = total)
+cohen.d(Shannon.s.Diversity~condoms.48h, data = total)
+
+#Pregnancy
+t.test(Shannon.s.Diversity~Pregnancy.cat, data = total)
+cohen.d(Shannon.s.Diversity~Pregnancy.cat, data = total)
+
+#Product use
+t.test(Shannon.s.Diversity~Feminine.products, data = total)
+cohen.d(Shannon.s.Diversity~Feminine.products, data = total)
+t.test(Shannon.s.Diversity~Feminine.products.48hrs, data = total)
+cohen.d(Shannon.s.Diversity~Feminine.products.48hrs, data = total)
+t.test(Shannon.s.Diversity~Tampon.Use.cat, data = total)
+cohen.d(Shannon.s.Diversity~Tampon.Use.cat, data = total)
+t.test(Shannon.s.Diversity~Tampon.use.1mth, data = total)
+cohen.d(Shannon.s.Diversity~Tampon.use.1mth, data = total)
+
+#substance use
+t.test(Shannon.s.Diversity~Substance.Use, data = total)
+cohen.d(Shannon.s.Diversity~Substance.Use, data = total)
+t.test(Shannon.s.Diversity~smoking.current, data = total)
+cohen.d(Shannon.s.Diversity~smoking.current, data = total)
+
+#adjust p-values; Benjamini Hochburg
+pvals <- read.csv(file = "clipboard") #copied from excel
+pvals
+
+a <- p.adjust(pvals$x, method = 'hochberg', n = 104)
+View(a)
+
+####
