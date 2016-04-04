dca6f5e920172f9f97055bebb2ed3f3879260724
KeshiniD <kdevakan@alumni.uwo.ca>
2016-04-03 17:54:01 -0700

    converted into factors

--- 1B_metadata_hpv_analyses.R
+++ 1B_metadata_hpv_analyses.R
@@ -11,63 +11,97 @@ total <- read.csv(file="1B_grouped.csv")
 
 #convert appropriate variables into factor, integers
 total$t06 <- factor(total$t06)
-
-"                                  "t11"                                 
-[3] "t16"                                  "t18"                                 
-[5] "t26"                                  "t31"                                 
-[7] "t33"                                  "t34"                                 
-[9] "t35"                                  "t39"                                 
-[11] "t40"                                  "t42"                                 
-[13] "t44"                                  "t45"                                 
-[15] "t51"                                  "t52"                                 
-[17] "t53"                                  "t54"                                 
-[19] "t56"                                  "t58"                                 
-[21] "t59"                                  "t61"                                 
-[23] "t62"                                  "t66"                                 
-[25] "t67"                                  "t68"                                 
-[27] "t69"                                  "t70"                                 
-[29] "t71"                                  "t72"                                 
-[31] "t73"                                  "t81"                                 
-[33] "t82"                                  "t83"                                 
-[35] "t84"                                  "t89"                                 
-[37] "Number.of.Different.HPV.Types"        "CST"                                 
-[39] "Age.cat"                              "BMI.under.cat"                       
-[41] "BMI.over.cat"                         "Ethnicity.cat"                       
-[43] "Ethnicity2.cat"                       "bv_life"                             
-[45] "bv_infecttotal_1yr"                   "bv_infecttotal_2mo"                  
-[47] "BV.ever"                              "Yeast.ever"                          
-[49] "UTI.ever"                             "Trich.ever"                          
-[51] "Condyloma.ever"                       "GenHerpes.ever"                      
-[53] "Chlamydia.ever"                       "Gonorrhea.ever"                      
-[55] "Syphillis.ever"                       "Presence.Symptoms.2wks"              
-[57] "Presence.Symptoms.48hrs"              "Symptom.pain"                        
-[59] "oralsxfrequency.cat"                  "analsxfrequency.cat"                 
-[61] "sextoyfrequency.cat"                  "sexpartner1yr.cat"                   
-[63] "Contraception.H"                      "Contraception.B.M"                   
-[65] "Contraception.IUD"                    "Contraception.none"                  
-[67] "condoms.48h"                          "Pregnancy.cat"                       
-[69] "Feminine.products"                    "Feminine.products.48hrs"             
-[71] "Tampon.Use.cat"                       "days.since.LMP"                      
-[73] "Tampon.use.1mth"                      "smoking.current"                     
-[75] "druguse"                              "substanceuse"                        
-[77] "Med.Duration"                         "Is.the.patient.antiretroviral.naive."
-[79] "HIV.Clade...Result"                   "Likely.mode.of.HIV.acquisition"      
-[81] "Duration.of.HIV.Infection."           "CD4.Nadir."                          
-[83] "Highest.VL.Ever.."                    "CD4."                                
-[85] "VL..copies.mL.."                      "HCV.Antibody...Result"               
-[87] "HCV.PCR...Result"                     "HBV.sAb...Result"                    
-[89] "HBV.sAg...Result"                     "HBV.cAb...Result"                    
-[91] "nugent_score_result"                  "sexpartner"                          
-[93] "contramethnotactive___1"              "abnormaldischarge2wk"                
-[95] "abnormaldischarge48"                  "abnormalodor2wk"                     
-[97] "abnormalodor48"                       "irritationdiscomfort2wk"             
-[99] "irritationdiscomfort48"               "vaginalsymptomother2wk"              
-[101] "vaginalsymptomother48" 
-
-
-
-
-
+total$t11 <- factor(total$t11)
+total$t16 <- factor(total$t16)
+total$t18 <- factor(total$t18)
+total$t26 <- factor(total$t26)
+total$t31 <- factor(total$t31)
+total$t33 <- factor(total$t33)
+total$t34 <- factor(total$t34)
+total$t35 <- factor(total$t35)
+total$t39 <- factor(total$t39)
+total$t40 <- factor(total$t40)
+total$t42 <- factor(total$t42)
+total$t44 <- factor(total$t44)
+total$t45 <- factor(total$t45)
+total$t51 <- factor(total$t51)
+total$t52 <- factor(total$t52)
+total$t53 <- factor(total$t53)
+total$t54 <- factor(total$t54)
+total$t56 <- factor(total$t56)
+total$t58 <- factor(total$t58)
+total$t59 <- factor(total$t59)
+total$t61 <- factor(total$t61)
+total$t62 <- factor(total$t62)
+total$t66 <- factor(total$t66)
+total$t67 <- factor(total$t67)
+total$t68 <- factor(total$t68)
+total$t69 <- factor(total$t69)
+total$t70 <- factor(total$t70)
+total$t71 <- factor(total$t71)
+total$t72 <- factor(total$t72)
+total$t73 <- factor(total$t73)
+total$t81 <- factor(total$t81)
+total$t82 <- factor(total$t82)
+total$t83 <- factor(total$t83)
+total$t84 <- factor(total$t84)
+total$t89 <- factor(total$t89)
+total$CST <- factor(total$CST)
+total$Age.cat <- factor(total$Age.cat)
+total$BMI.under.cat <- factor(total$BMI.under.cat)
+total$BMI.over.cat <- factor(total$BMI.over.cat)
+total$Ethnicity.cat <- factor(total$Ethnicity.cat)
+total$BV.ever <- factor(total$BV.ever)
+total$Yeast.ever <- factor(total$Yeast.ever)
+total$UTI.ever <- factor(total$UTI.ever)
+total$Ethnicity2.cat <- factor(total$Ethnicity2.cat)
+total$Trich.ever <- factor(total$Trich.ever)
+total$Condyloma.ever <- factor(total$Condyloma.ever)
+total$GenHerpes.ever <- factor(total$GenHerpes.ever)
+total$Chlamydia.ever <- factor(total$Chlamydia.ever)
+total$Gonorrhea.ever <- factor(total$Gonorrhea.ever)
+total$Syphillis.ever <- factor(total$Syphillis.ever)
+total$Presence.Symptoms.2wks <- factor(total$Presence.Symptoms.2wks)
+total$Presence.Symptoms.48hrs <- factor(total$Presence.Symptoms.48hrs)
+total$Symptom.pain <- factor(total$Symptom.pain)
+total$oralsxfrequency.cat <- factor(total$oralsxfrequency.cat)
+total$analsxfrequency.cat <- factor(total$analsxfrequency.cat)
+total$sextoyfrequency.cat <- factor(total$sextoyfrequency.cat)
+total$sexpartner1yr.cat <- factor(total$sexpartner1yr.cat)
+total$Contraception.H <- factor(total$Contraception.H)
+total$Contraception.B.M <- factor(total$Contraception.B.M)
+total$Contraception.IUD <- factor(total$Contraception.IUD)
+total$Contraception.none <- factor(total$Contraception.none)
+total$condoms.48h <- factor(total$condoms.48h)
+total$Pregnancy.cat <- factor(total$Pregnancy.cat)
+total$Feminine.products <- factor(total$Feminine.products)
+total$Feminine.products.48hrs <- factor(total$Feminine.products.48hrs)
+total$Tampon.Use.cat <- factor(total$Tampon.Use.cat)
+total$Tampon.use.1mth <- factor(total$Tampon.use.1mth)
+total$smoking.current <- factor(total$smoking.current)
+total$druguse <- factor(total$druguse)
+total$substanceuse <- factor(total$substanceuse)
+total$Is.the.patient.antiretroviral.naive. <- factor(total$Is.the.patient.antiretroviral.naive.)
+total$HIV.Clade...Result <- factor(total$HIV.Clade...Result)
+total$Likely.mode.of.HIV.acquisition <- factor(total$Likely.mode.of.HIV.acquisition)
+total$HCV.Antibody...Result <- factor(total$HCV.Antibody...Result)
+total$HCV.PCR...Result <- factor(total$HCV.PCR...Result)
+total$HBV.sAb...Result <- factor(total$HBV.sAb...Result)
+total$HBV.sAg...Result <- factor(total$HBV.sAg...Result)
+total$HBV.cAb...Result <- factor(total$HBV.cAb...Result)
+total$nugent_score_result <- factor(total$nugent_score_result)
+total$sexpartner <- factor(total$sexpartner)
+total$contramethnotactive___1 <- factor(total$contramethnotactive___1)
+total$abnormaldischarge2wk <- factor(total$abnormaldischarge2wk)
+total$abnormaldischarge48 <- factor(total$abnormaldischarge48)
+total$abnormalodor2wk <- factor(total$abnormalodor2wk)
+total$abnormalodor48 <- factor(total$abnormalodor48)
+total$irritationdiscomfort2wk <- factor(total$irritationdiscomfort2wk)
+total$irritationdiscomfort48 <- factor(total$irritationdiscomfort48)
+total$vaginalsymptomother2wk <- factor(total$vaginalsymptomother2wk)
+total$vaginalsymptomother48 <- factor(total$vaginalsymptomother48)
 
 #t06, t11, 34, 39, 44, 69, 82
 #have only 1 factor level (0 participants have these types)
+
+#nugent (consistent -1, intermediate -2, inconsistent -3)
