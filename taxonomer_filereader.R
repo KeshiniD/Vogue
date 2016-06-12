90a8e6c3c2c26d666b8296b5c9d492b5e80759bc
KeshiniD <kdevakan@alumni.uwo.ca>
2016-06-12 14:51:44 -0700

    taxonomer data

--- taxonomer_filereader.R
+++ taxonomer_filereader.R
@@ -1,10 +1,17 @@
 #to read taxonomer files for virome, subset only viral species
 
-a <- read.delim(file = "Vogue1B_01_RNA_1.full.txt")
+a <- read.delim(file = "Vogue1A_101_RNA_1.full.taxonomer")
 library(plyr)
 suppressPackageStartupMessages(library(dplyr)) 
 library(tidyr)
 library(knitr)
 
 a2  <-  a[which(a$bacterial == 'viral'),]
-write.csv(a2, "virometemp1b.csv")
+write.csv(a2, "Vogue1A_101_RNA_viromedata.csv")
+
+###
+
+a <- read.delim(file = "Vogue1B_01_RNA_1.full (1).taxonomer")
+colnames(a)
+a2  <-  a[which(a$bacterial == 'viral'),]
+write.csv(a2, "Vogue1B_01_RNA_viromedata.csv")
