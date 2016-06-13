35f9907286fc1486231b0fcbd806dbafc15a6f6b
KeshiniD <kdevakan@alumni.uwo.ca>
2016-06-13 11:58:11 -0700

    read taxonomy ids

--- bash_virome_taxonomerreader.R
+++ bash_virome_taxonomerreader.R
@@ -3,3 +3,7 @@ for f in *.taxonomer; do name=$(echo $f | cut -f1 -d"."); grep "^viral" $f | gaw
 
 #enter
 #will be in viromefastq folder
+
+#taxonomer reader
+a <- taxize::ncbi_get_taxon_summary(c(13097, 13098, 3554))
+#instead of giving numbers, do data$colname with numbers in it
