4c079e8912d6a0e837dbd5338e6410ecdb2c897f
KeshiniD <kdevakan@alumni.uwo.ca>
2016-06-17 13:49:23 -0700

    file transfer to salk

--- bash_virome_taxonomerreader.R
+++ bash_virome_taxonomerreader.R
@@ -7,3 +7,7 @@ for f in *.taxonomer; do name=$(echo $f | cut -f1 -d"."); grep "^viral" $f | gaw
 #taxonomer reader
 a <- taxize::ncbi_get_taxon_summary(c(13097, 13098, 3554))
 #instead of giving numbers, do data$colname with numbers in it
+
+#transfer files to salk
+#set working directory to desktop
+scp ~/Desktop/ViromeFASTQ_all/DNA_Viruses/C935CANXX_7_DNA/Controls/DNA_Adeno/* kdevakandan@salk.bccdc.med.ubc.ca:/home/kdevakandan/virome_kesh/
