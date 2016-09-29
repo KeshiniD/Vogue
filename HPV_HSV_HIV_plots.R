acc4bb642de6fa57478345fd15d189bc70c13ed1
KeshiniD <kdevakan@alumni.uwo.ca>
2016-09-28 18:38:09 -0700

    three major virus proportions

---
+++ HPV_HSV_HIV_plots.R
@@ -0,0 +1,6 @@
+#plot
+#call data
+everyone <- read.csv("HPV_HSV_HIV_all.csv")
+vogueA <- read.csv("HPV_HSV_HIV_1A.csv")
+vogueB <- read.csv("HPV_HSV_HIV_1B.csv")
+vogue1b2 <- read.csv("HPV_HSV_HIV_1B2.csv")
