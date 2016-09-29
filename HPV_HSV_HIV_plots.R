a0e8170c12e4213b593ca0728a75374009dd8b04
KeshiniD <kdevakan@alumni.uwo.ca>
2016-09-28 20:22:46 -0700

    edits to format

--- HPV_HSV_HIV_plots.R
+++ HPV_HSV_HIV_plots.R
@@ -4,3 +4,6 @@ everyone <- read.csv("HPV_HSV_HIV_all.csv")
 vogueA <- read.csv("HPV_HSV_HIV_1A.csv")
 vogueB <- read.csv("HPV_HSV_HIV_1B.csv")
 vogue1b2 <- read.csv("HPV_HSV_HIV_1B2.csv")
+
+#to order alphabetically
+a <- a[order(a$Viral_Species),]
